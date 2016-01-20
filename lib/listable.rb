7f3ee48cd57e5ed6464a7868efb67c95674642b5
James Teitsworth <james.teitsworth@outlook.com>
2016-01-19 21:53:07 -0600

    Add date and priority formatting to listable mixin.

--- lib/listable.rb
+++ lib/listable.rb
@@ -2,4 +2,24 @@ module Listable
   def format_description(description)
     "#{description}".ljust(30)
   end
+
+  def format_date(type, options = {})
+    if type == :single_date
+      return options[:due] ? options[:due].strftime("%D") : "No due date"
+    end
+    if type == :dual_dates
+      dates = options[:start_date].strftime("%D") if options[:start_date]
+      dates << " -- " + options[:end_date].strftime("%D") if options[:end_date]
+      dates = "N/A" if !dates
+      return dates
+    end
+  end
+
+  def format_priority(priority)
+    value = " ⇧" if priority == "high"
+    value = " ⇨" if priority == "medium"
+    value = " ⇩" if priority == "low"
+    value = "" if !priority
+    return value
+  end
 end
